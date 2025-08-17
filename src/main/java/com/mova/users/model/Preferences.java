8142f51f4c11a813dc7fe4adb59da29f713a5bee
Jesus Santiago <jesus-contreras1@eep-igroup.com>
2025-08-17 16:02:58 +0200

    MV-86-[FIX]- añadí constructores, getters and setters

--- src/main/java/com/mova/users/model/Preferences.java
+++ src/main/java/com/mova/users/model/Preferences.java
@@ -4,4 +4,37 @@ public class Preferences {
     private String language = "es";
     private Boolean darkMode = false;
     private String currency = "EUR";
+
+    public Preferences() {
+    }
+
+    public Preferences(String language, Boolean darkMode, String currency) {
+        this.language = language;
+        this.darkMode = darkMode;
+        this.currency = currency;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
+    public Boolean getDarkMode() {
+        return darkMode;
+    }
+
+    public void setDarkMode(Boolean darkMode) {
+        this.darkMode = darkMode;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
 }
--- src/main/java/com/mova/users/model/User.java
+++ src/main/java/com/mova/users/model/User.java
@@ -31,4 +31,78 @@ public class User {
             joinColumns = @JoinColumn(name = "uid"),
             inverseJoinColumns = @JoinColumn(name = "role_name"))
     private Set<Role> roles;
+
+    public User() {
+    }
+
+    public User(String uid, String email, String displayName, String photo) {
+        this.uid = uid;
+        this.email = email;
+        this.displayName = displayName;
+        this.photo = photo;
+    }
+
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
+    }
+
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public void setDisplayName(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public String getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(String photo) {
+        this.photo = photo;
+    }
+
+    public Preferences getPreferences() {
+        return preferences;
+    }
+
+    public void setPreferences(Preferences preferences) {
+        this.preferences = preferences;
+    }
+
+    public Instant getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Instant createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Instant getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Instant updatedAt) {
+        this.updatedAt = updatedAt;
+    }
 }
